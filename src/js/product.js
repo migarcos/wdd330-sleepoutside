5ce31955c0433af38d959d548b3c3b69d902f283
migarcos <migark@hotmail.com>
2025-03-08 16:39:27 -0500

    w01 cart addition solved

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+//  *  1st W Changed  *   // import { setLocalStorage } from "./utils.mjs"; 
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+//  *  1st W Changed  * // function addProductToCart(product) { 
+//   setLocalStorage("so-cart", product);
+// }
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product); 
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
